710e62e158931f777d7f763af84e979ba33064bb
Taylor Farley <twfarley88@gmail.com>
2020-08-19 13:09:44 -0400

    fixing 404 error

--- src/app.js
+++ src/app.js
@@ -12,8 +12,9 @@ app.get('/', function (req, res) {
 })
 
 someFunction = (req, res) => {
+
     const x = process.env.API
-    fetch('https://api.nasa.gov/planetary/apod?api_key='+x)
+    fetch('https://api.nasa.gov/planetary/apod?api_key=' + x)
         .then(result => {
             return result.json()
         })
@@ -22,9 +23,18 @@ someFunction = (req, res) => {
                 image: data.url,
                 descript: data.explanation,
             }
-            res.send({
-                foo
-            });
+            var n = foo.image.includes('jpg')
+            if (n) {
+                res.send({
+                    foo
+                });
+            } else {
+                foo.image = "https://apod.nasa.gov/apod/image/1906/OrionDeep_Klinger_960.jpg"
+                foo.descript = "The constellation of Orion is much more than three stars in a row. It is a direction in space that is rich with impressive nebulas. To better appreciate this well-known swath of sky, a new  long exposure image was taken over several clear nights in January, February and March. After 23 hours of camera time and untold hours of image processing, the featured collage in the light of hydrogen, oxygen, and sulfur was produced spanning over 40 times the angular diameter of the Moon.  Of the many interesting details that have become visible, one that particularly draws the eye is Barnard's Loop."
+                res.send({
+                    foo
+                })
+            }
         })
         .catch(error => console.log(error))
 }
